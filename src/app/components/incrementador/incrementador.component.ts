637206af8a6dd0d1dfae1c9ddfaf7e01f200502c
Panasony <panasony.ing.uc@gmail.com>
2021-02-17 17:58:09 -0400

    comunicación entre componentes input y output

---
+++ src/app/components/incrementador/incrementador.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-incrementador',
+  templateUrl: './incrementador.component.html',
+  styleUrls: ['./incrementador.component.css']
+})
+export class IncrementadorComponent {
+
+  @Input('valor') progreso : number = 40;
+
+  @Output() valorSalida: EventEmitter<number> = new EventEmitter();
+
+  get getPorcentaje() {
+    return `${this.progreso}%`;
+  }
+
+  cambiarValor( valor: number) {
+
+    if( this.progreso >= 100 && valor >= 0){
+      this.valorSalida.emit(100);
+      return this.progreso = 100;
+    }
+
+    if( this.progreso <= 0 && valor < 0){
+      this.valorSalida.emit(0);
+      return this.progreso = 0;
+    }
+
+    this.progreso = this.progreso + valor;
+    this.valorSalida.emit(this.progreso);
+  };
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router';
 
 
 
@@ -19,7 +22,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    ComponentsModule
   ],
   exports: [
     DashboardComponent,
